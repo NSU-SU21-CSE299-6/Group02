12aa3c3f076147ea9918dae5b1de346d8998db40
B.M.M. BARI <b.bari@northsouth.edu>
2021-07-05 13:46:22 +0600

    Code #3: Initial code commit
    Index - Initial code commit

---
+++ Code/practice/index.php
@@ -0,0 +1,29 @@
+<?php
+  include_once 'header.php';
+
+?>
+
+
+<section class="index-intro">
+  <h1>Rent Tracker</h1>
+  <p>An Innovative Way to Track Your Rent!</p>
+</section>
+
+<section class="index-gallery">
+  <h2>Gallery</h2>
+  <div class="index-gallery-list">
+    <div>
+      <a href="index.php"><img src="img/index-img-1.png" alt="Image 1"></a>
+    </div>
+    <div>
+      <a href="index.php"><img src="img/index-img-2.png" alt="Image 2"></a>
+    </div>
+    <div>
+      <a href="index.php"><img src="img/index-img-3.png" alt="Image 3"></a>
+    </div>
+  </div>
+</section>
+
+<?php
+  include_once 'footer.php';
+?>
