2f8118fa82234bf5540264a39f6515fc6d3969e6
FairuzNawar <fairuz.nawar01@northsouth.edu>
2021-07-26 00:15:25 +0600

    enhancement #24: database update
    database updated

--- Code/practice/phpproject01.sql
+++ Code/practice/phpproject01.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2020 at 11:48 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.26
+-- Generation Time: Jul 25, 2021 at 06:22 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,104 +18,114 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sms`
+-- Database: `phpproject01`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login`
+-- Table structure for table `flat`
+--
+
+CREATE TABLE `flat` (
+  `FlatId` int(11) NOT NULL,
+  `FlatNo` varchar(255) DEFAULT NULL,
+  `UserStatus` varchar(255) DEFAULT NULL,
+  `VacancyStatus` varchar(255) DEFAULT NULL,
+  `FlatInfo` varchar(255) DEFAULT NULL,
+  `RentInfo` varchar(255) DEFAULT NULL,
+  `PabxNo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rent`
+--
+
+CREATE TABLE `rent` (
+  `TenantId` int(8) DEFAULT NULL,
+  `FlatNo` varchar(255) DEFAULT NULL,
+  `BillingMonth` varchar(255) DEFAULT NULL,
+  `PaymentDate` date DEFAULT NULL,
+  `Amount` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenant`
+--
+
+CREATE TABLE `tenant` (
+  `UserId` int(11) NOT NULL,
+  `UserName` varchar(255) NOT NULL,
+  `TenentId` varchar(255) DEFAULT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `EmailId` varchar(255) DEFAULT NULL,
+  `Contact` varchar(255) DEFAULT NULL,
+  `FlatNo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `usersId` int(11) NOT NULL,
-  `usersName` varchar(50) NOT NULL,
-  `usersEmail` varchar(250) NOT NULL,
-  `usersUid` varchar(128) NOT NULL,
-  `usersPwd` varchar(250) NOT NULL
+  `Id` int(11) NOT NULL,
+  `UserName` varchar(255) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `EmailId` varchar(255) DEFAULT NULL,
+  `Contact` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `login`
---
-
-INSERT INTO `users` (`usersId`, `usersName`, `usersEmail`, `usersUid`, `usersPwd`) VALUES
-(1, 'Admin', 'admin@gmail.com', 'admin12', 'admin12');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `students`
---
-
-CREATE TABLE `tenantinformation` (
-  `usersId` int(11) NOT NULL,
-  `tenantId` int(11) NOT NULL,
-  `usersName` varchar(100) NOT NULL,
-  `flatNo` varchar(100) NOT NULL,
-  `mobile` varchar(25) NOT NULL,
-  `usersEmail` varchar(100) NOT NULL,
-  `usersUid` varchar(128) NOT NULL,
-  `usersPwd` varchar(250) NOT NULL,
-  `rent` varchar(100) NOT NULL,
-  `nid` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `students`
---
-
-INSERT INTO `tenantinformation` (`usersId`, `tenantId`, `usersName`, `flatNo`, `mobile`, `usersEmail`, `usersUid`,`usersPwd`, `rent`, `nid`) VALUES
-(5, 1002, 'Abul', '1', '01759364574', 'abul@gmail.com', 'abul12', 'abul12', '10000', '5789638'),
-(7, 1003, 'Kabul', '2', '0171968574', 'kabul@gmail.com', 'kabul12', 'kabul12', '10500', '5789639'),
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teachers`
---
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `login`
+-- Indexes for table `flat`
+--
+ALTER TABLE `flat`
+  ADD PRIMARY KEY (`FlatId`);
+
+--
+-- Indexes for table `tenant`
+--
+ALTER TABLE `tenant`
+  ADD PRIMARY KEY (`UserId`);
+
+--
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`usersId`);
-
---
--- Indexes for table `students`
---
-ALTER TABLE `tenantinformation`
-  ADD PRIMARY KEY (`usersId`);
-
---
--- Indexes for table `teachers`
---
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `login`
+-- AUTO_INCREMENT for table `flat`
+--
+ALTER TABLE `flat`
+  MODIFY `FlatId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tenant`
+--
+ALTER TABLE `tenant`
+  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `usersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- AUTO_INCREMENT for table `students`
---
-ALTER TABLE `tenantinformation`
-  MODIFY `usersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- AUTO_INCREMENT for table `teachers`
---
-
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
