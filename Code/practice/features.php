3142126e034c471d9eaccf698452bf94595d082a
Eh-Abir <amdadul.haque17@northsouth.edu>
2021-07-25 18:21:47 +0600

    enhancement #16 design a feature page
    Feature Page Created

---
+++ Code/practice/features.php
@@ -0,0 +1,16 @@
+
+
+<section class="howItWorks">
+  <h1>Rent Tracker</h1>
+  <p>User can use following features - </p>
+
+  <ol>
+    <li><a href="index.php">Keep Track on the Rent</a></li>
+    <li><a href="index.php">Tenants Information</a></li>
+    <li><a href="index.php">Utility Bill Summary</a></li>
+    <li><a href="index.php">Flat Informations</a></li>
+    <li><a href="index.php">Loan Installment</a></li>
+    <li><a href="index.php">Maintainance Bill</a></li>
+  </ol>
+</section>
+
