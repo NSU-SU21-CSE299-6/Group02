04e09d61d83916e64cff978e206b7e69702a7b23
Eh-Abir <amdadul.haque17@northsouth.edu>
2021-07-25 18:26:37 +0600

    Code: #26 about page update
    About page updated

--- Code/practice/about.php
+++ Code/practice/about.php
@@ -1,17 +1,11 @@
-<?php
-  include_once 'header.php';
-?>
+
 
 <section class="howItWorks">
   <h1>Rent Tracker</h1>
-  <p>User can use following features - </p>
+  <p>About Rentracker</p>
 
-  <ol>
-    <li><a href="index.php">Keep Track on the Rent</a></li>
-    <li><a href="index.php">Tenants Information</a></li>
-    <li><a href="index.php">Utility Bill Summary</a></li>
-    <li><a href="index.php">Flat Informations</a></li>
-    <li><a href="index.php">Loan Installment</a></li>
-    <li><a href="index.php">Maintainance Bill</a></li>
-  </ol>
-</section
+  <h3>“House Rental Management System” is a web based application. This application stores the information of tenants, rent information of every month. Admin and user both can use this application. Everyone can search,store and get information by having a Unique ID. Each ID / Person / User / Admin will have their own profile with sign in ID so that the information can be secured. Today we are living in the first world where everyone uses a device. Everyone uses a software or application to store data and receive information. The world is surrounded by software and applications.</h3>
+
+
+</section>
+
