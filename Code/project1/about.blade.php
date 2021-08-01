a501a6bc400d5ed6db5552ab99b7a42105a358ab
B.M.M. BARI <b.bari@northsouth.edu>
2021-08-02 03:30:50 +0600

    frontend #31: about page design
    Basic about page designed, further it needs to be updated.

---
+++ Code/project1/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+    @section('content')
+       <br><br><h1>{{$title}}</h1>
+       <p>“House Rental Management System” is a web based application. This application stores the information of
+         tenants, rent information of every month. Admin and user both can use this application. Everyone can search,
+         store and get information by having a Unique ID. Each ID / Person / User / Admin will have their own profile
+         with sign in ID so that the information can be secured. Today we are living in the first world where everyone
+         uses a device. Everyone uses a software or application to store data and receive information. The world is
+         surrounded by software and applications </p>
+    @endsection
