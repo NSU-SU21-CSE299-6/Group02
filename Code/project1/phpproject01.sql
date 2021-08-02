f69aaf8cdf2e7aeee2a7099774ea053d91cda825
FairuzNawar <fairuz.nawar01@northsouth.edu>
2021-08-02 12:02:06 +0600

    enhancement #28: database update
    database is updated according to main project

---
+++ Code/project1/phpproject01.sql
@@ -0,0 +1,132 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 02, 2021 at 07:55 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpproject01`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flat`
+--
+
+CREATE TABLE `flat` (
+  `FlatId` int(11) NOT NULL,
+  `FlatNo` varchar(255) DEFAULT NULL,
+  `UserStatus` varchar(255) DEFAULT NULL,
+  `VacancyStatus` varchar(255) DEFAULT NULL,
+  `FlatInfo` varchar(255) DEFAULT NULL,
+  `RentInfo` varchar(255) DEFAULT NULL,
+  `PabxNo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rent`
+--
+
+CREATE TABLE `rent` (
+  `TenantId` int(8) DEFAULT NULL,
+  `FlatNo` varchar(255) DEFAULT NULL,
+  `BillingMonth` varchar(255) DEFAULT NULL,
+  `PaymentDate` date DEFAULT NULL,
+  `Amount` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenant`
+--
+
+CREATE TABLE `tenant` (
+  `UserId` int(11) NOT NULL,
+  `UserName` varchar(255) NOT NULL,
+  `TenentId` varchar(255) DEFAULT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `EmailId` varchar(255) DEFAULT NULL,
+  `Contact` varchar(255) DEFAULT NULL,
+  `FlatNo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL,
+  `UserName` varchar(255) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `EmailId` varchar(255) DEFAULT NULL,
+  `Contact` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `flat`
+--
+ALTER TABLE `flat`
+  ADD PRIMARY KEY (`FlatId`);
+
+--
+-- Indexes for table `tenant`
+--
+ALTER TABLE `tenant`
+  ADD PRIMARY KEY (`UserId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `flat`
+--
+ALTER TABLE `flat`
+  MODIFY `FlatId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tenant`
+--
+ALTER TABLE `tenant`
+  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
