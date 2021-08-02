f0f3b67738c767767374ae0b920c2ccfcbb1dd74
FairuzNawar <fairuz.nawar01@northsouth.edu>
2021-08-02 12:00:56 +0600

    frontend #35: navigation bar
    a basic navigation bar is completed

---
+++ Code/project1/navbar.blade.php
@@ -0,0 +1,15 @@
+<div class="header">
+    <a href="#default"><img src="img/logo.png" alt="Page logo"></a>
+    <div class="header-left">
+        
+        <a class="active" href="/">Home</a>
+        <a Class="active" href="/about">About</a>
+        <a Class="active" href="/services">Services</a>
+        <a Class="active" href="/features">Features</a>
+
+    </div>
+    <div class="header-right">
+        <a class="active" href="/">Sign In</a>
+        <a Class="active" href="/about">Sign Up</a>
+      </div>
+  </div>
