d698c47a83f8360f956560c17cda09c74a342703
B.M.M. BARI <b.bari@northsouth.edu>
2021-08-13 00:51:04 +0600

    frontend #43 navigation bar
    navigation bar updated.

--- Code/house_rent_track/resources/views/layouts/header.blade.php
+++ Code/house_rent_track/resources/views/layouts/header.blade.php
@@ -1,24 +1,28 @@
 <!-- Navigation -->
 <nav class="top-menu-container">
-                <div class="logo-header">
-                    <a href="">
-                        <img 
-                        src="https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png" 
-                        alt="Logo personal portfolio"
-                        title="Logo personal portfolio"
-                        />
-                    </a>
-                </div>
+            <div class="logo-header">
+                <a href="">
+                    <img 
+                    src="https://media.istockphoto.com/photos/real-estate-houses-isometric-logo-design-blue-color-3d-rendering-picture-id1175964613" 
+                    alt="Logo"
+                    title="Logo"
+           
+                    />
+                </a>
+            </div>
 
-                <ul>
-                <li>
-                    <a href="">About</a>
-                </li>
-                <li>
-                    <a href="">Portfolio</a>
-                </li>
-                <li>
-                    <a href="">Contact</a>
-                </li>
-            </ul>
-        </nav>
+            <ul>
+            <li>
+                <a href="/" >Home</a>
+            </li>
+            <li>
+                <a href="about">About</a>
+            </li>
+            <li>
+                <a href="feature">Features</a>
+            </li>
+            <li>
+                <a href="">Contact</a>
+            </li>
+        </ul>
+    </nav>
