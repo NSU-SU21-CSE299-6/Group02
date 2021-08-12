30e25e1b20811b44d0ee590fde9b04531d5da746
B.M.M. BARI <b.bari@northsouth.edu>
2021-08-13 00:53:20 +0600

    frontend #46 design contact page
    Contact page design

---
+++ Code/house_rent_track/resources/views/contact.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <!-- Contact Page-->
+    <div id="hero-container">
+        <div class="hero-wrapper">
+            <h1> <span class="orange-txt">Welcome to House Rental Management System</span></h1>
+            <h1> "A Conveninent Way To Track Your Rent" </h1>
+            <div class="btn-wrapper">
+                <a href="">Sign In </a>
+                <a href="">Sign Up</a>
+            </div>
+        </div>
+    </div>
+
+    
+
+
+@endsection
