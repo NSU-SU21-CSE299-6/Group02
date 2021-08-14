7dea0ec5a866d085db1f5a34fd2bd93babd32a4c
B.M.M. BARI <b.bari@northsouth.edu>
2021-08-14 23:46:41 +0600

    frontend #45 Design Feature Page
    Feature Page Design Complete.

--- Code/house_rent_track/resources/views/feature.blade.php
+++ Code/house_rent_track/resources/views/feature.blade.php
@@ -4,58 +4,107 @@
 @section('content')
 
 
-<!-- Section of skills -->
-<div class="section-why-us">
+
+<!-- Section of Features -->
+<div class="header-section">
+    <h2 class="dark big">Features</h2>
+    
+    <hr>
+</div>
+<div class="section-features">
     <div>
-        <i class="fas fa-code-branch icon-why-us"></i>            
+        <i class="fas fa-calculator fa-5x"></i>            
         <h2>
-            Quality Control
+            Track Rent
         </h2>
         <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+           Keep Track On Your Rrnt.
         </p>
     </div>
 
-    <div class="middle">
+    <div>
+        <i class="far fa-address-card fa-5x"></i>   
         <h2>
-            <img src="img/icon-box.jpg" alt="">
+            Tenant Information
         </h2>
+        <p>
+            All The Necessary Information About Tenant. 
+        </p>
     </div>
 
     <div>
-        <i class="far fa-keyboard icon-why-us"></i>   
+        <i class="fas fa-home fa-5x"></i>   
         <h2>
-            Optional Maintenance
+            Flat Information        
         </h2>
         <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            All The Necessary Information About Flats. 
+        </p>
+    </div>
+
+    <div>
+        <i class="far fa-clipboard fa-5x"></i>   
+        <h2>
+            Vacancy
+        </h2>
+        <p>
+            Flat Vacancy Status. 
+        </p>
+    </div>
+
+    <div>
+        <i class="fab fa-amazon-pay fa-5x"></i>   
+        <h2>
+            Utility Bills.
+        </h2>
+        <p>
+            Pay Your Utility Bill Online. 
+        </p>
+    </div>
+    <!--<div class="middle">
+        <h2>
+            <img src="img/icon-box.jpg" alt="alt image">
+        </h2>
+    </div> -->
+
+    <div>
+        <i class="fas fa-university fa-5x"></i>   
+        <h2>
+            House Installment
+        </h2>
+        <p>
+            House Loan Installment. 
         </p>
     </div>
     
     <div>
-        <i class="fab fa-google icon-why-us"></i>            
+        <i class="fas fa-bell fa-5x"></i>            
         <h2>
-            Search Engine Friendly
+            Notice
         </h2>
         <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            All The Valuable Notices.
         </p>
     </div>
     
     <div>
-        <i class="fas fa-globe icon-why-us"></i>            
+        <i class="fas fa-image fa-5x"></i>            
         <h2>
-            Web Master Tools
+            Gallery
         </h2>
         <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Moments.
         </p>
     </div>
 </div>
 
+
 <!-- 4 grid layout skills on homepage -->
     <!-- Every div is a grid item -->
-    <div class="container-grid-4">
+
+    <!-- Wiil Use Later  
+    
+        <div class="container-grid-4">
         <div>
             <img src="{{ URL('storage/icon-box.jgp') }}" alt="picture">
             <h2>
@@ -95,13 +144,9 @@
                 Lorem ipsum dolor sit amet consectetur!
             </p>
         </div>
-    </div>
+    </div>  -->
 
-    <div class="header-section">
-        <h2 class="dark big">Team</h2>
-        
-        <hr>
-    </div>
+  
 
 
 @endsection
--- Code/house_rent_track/resources/views/layouts/footer.blade.php
+++ Code/house_rent_track/resources/views/layouts/footer.blade.php
@@ -3,6 +3,7 @@
                 src="https://media.istockphoto.com/photos/real-estate-houses-isometric-logo-design-blue-color-3d-rendering-picture-id1175964613" 
                 alt="Logo"
                 title="Logo"
+                height="40" width="100"
                 />
             <p>
                 This website is created by Team Scorpion
