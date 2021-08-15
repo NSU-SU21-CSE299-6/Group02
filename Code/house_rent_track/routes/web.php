251c83d0131d4f2768f550aeac76cce1d4e96c76
B.M.M. BARI <b.bari@northsouth.edu>
2021-08-16 03:24:54 +0600

    backend #48 authentication file created by laravel
    files uploaded.

--- Code/house_rent_track/routes/web.php
+++ Code/house_rent_track/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,15 +15,26 @@ use App\Http\Controllers\PagesController;
 |
 */
 
+
+
+
+
 /*Route::get('/', 'App\Http\Controllers\PagesController@index');
 Route::get('/about', 'App\Http\Controllers\PagesController@about');
 Route::get('/services', 'App\Http\Controllers\PagesController@services');
 Route::get('/features', 'App\Http\Controllers\PagesController@features');*/
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
 Route::get('/', [PagesController::class, 'index']);
 Route::get('/about', [PagesController::class, 'about']);
 Route::get('/feature', [PagesController::class, 'feature']);
 Route::get('/contact', [PagesController::class, 'contact']);
+Route::get('/admindash', [PagesController::class, 'admindash']);
 
 /*
 Route::get('/', function () {
