993b38f623f0ef6588bf4dcdca079115704ac089
B.M.M. BARI <b.bari@northsouth.edu>
2021-08-16 03:31:11 +0600

    frontend #31 about page update
    update about page.

--- Code/house_rent_track/resources/views/about.blade.php
+++ Code/house_rent_track/resources/views/about.blade.php
@@ -4,37 +4,39 @@
 @section('content')
 
      <!--About -->
-     <div class="container-grid-2">
-        <div>
-            <img class="img-team" src="{{ URL('storage/team-section-home.jgp') }}" alt="picture">
-        </div>
-        <div>
-            <h2>
-                Who we are
-            </h2>
 
-            <h3 class="orange-txt">
-                Meet Our Team
-            </h3>
-
-            <p>
-                Whether you require distribution or fulfillment, defined freight forwarding, or a complete supply chain solution, we are here for you.
-            </p>
-            
-            <div class="btn-wrapper">
-                <a href="">About</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="header-section">
-        <h2 class="dark big">Skills</h2>
+     <div class="header-section">
+        <h2 class="dark big">About</h2>
 
         <hr>
     </div>
 
-    
+     <div class="container-grid-2">
+        <div>
+            <img class="img-team" src="{{ URL('storage/1.png') }}" alt="picture">
+        </div>
+        <div>
+            <h2>
+                House Rental Management System
+            </h2>
 
+            <h3 class="orange-txt">
+                Introduction
+            </h3>
 
-
+            <p>
+                “House Rental Management System” is a web based application. This application stores the information of
+                    tenants, rent information of every month. Admin and user both can use this application. Everyone can search,
+                    store and get information by having a Unique ID. Each ID / Person / User / Admin will have their own profile
+                    with sign in ID so that the information can be secured. Today we are living in the first world where everyone
+                    uses a device. Everyone uses a software or application to store data and receive information. The world is
+                    surrounded by software and applications.
+            </p>
+            
+            <div class="btn-wrapper">
+                <a href="feature">Features</a>
+            </div>
+        </div>
+    </div>
+   
 @endsection
--- Code/house_rent_track/resources/views/index.blade.php
+++ Code/house_rent_track/resources/views/index.blade.php
@@ -9,7 +9,7 @@
             <h1> <span class="orange-txt">Welcome to House Rental Management System</span></h1>
             <h1> "A Conveninent Way To Track Your Rent" </h1>
             <div class="btn-wrapper">
-                <a href="">Sign In </a>
+                <a href="dashboard">Sign In </a>
                 <a href="">Sign Up</a>
             </div>
         </div>
